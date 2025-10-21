ed764838b706883fa6b78760a17b5c4309daa12e
shibil <shibilveettilakath6005@gmail.com>
2025-10-21 01:00:22 -0400

    Add files via upload

---
+++ nova/script.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My Nova Project with JS</title>
+</head>
+<body>
+    <h1>Welcome to My Nova Project!</h1>
+    <p id="demo">This will be changed by JavaScript.</p>
+
+    <!-- External JavaScript -->
+    <script src="script.js"></script>
+</body>
+</html>
